d20cc12fe6aaae7b8ab6041e404808a8b9a05991
Otti B <ottavio.braun@googlemail.com>
2025-05-04 13:12:35 +0200

    MOD changing layout [WORKING] make it simpel

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,14 +10,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="relative bg-black text-white">
-        {/* FIXED LOGO OBEN RECHTS */}
-        <div className="fixed top-4 right-4 z-50 w-[60px] h-[60px]">
-          <img
-            src="/Logo_trans.png"
-            alt="SYNTX Logo"
-            className="w-full h-full object-contain drop-shadow-[0_0_10px_rgba(255,255,255,0.3)]"
-          />
-        </div>
+        {/*/!* FIXED LOGO OBEN RECHTS *!/*/}
+        {/*<div className="fixed top-4 right-4 z-50 w-[60px] h-[60px]">*/}
+        {/*  <img*/}
+        {/*    src="/Logo_trans.png"*/}
+        {/*    alt="SYNTX Logo"*/}
+        {/*    className="w-full h-full object-contain drop-shadow-[0_0_10px_rgba(255,255,255,0.3)]"*/}
+        {/*  />*/}
+        {/*</div>*/}
         {children}
       </body>
     </html>
